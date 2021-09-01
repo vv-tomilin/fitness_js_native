ccd9482a23b49659781bb7636a11413c658f2d1f
vv_tomilin <backatom1@gmail.com>
2021-09-01 12:05:07 +0500

    Изменил файл инструкций для Gulp и настроил его

--- gulpfile.js
+++ gulpfile.js
@@ -1,107 +1,152 @@
-"use strict";
+let project_folder = require("path").basename(__dirname);
+let source_folder = "src";
 
-const gulp = require("gulp");
-const webpack = require("webpack-stream");
-const browsersync = require("browser-sync");
-const sass = require("gulp-sass");
-const autoprefixer = require("autoprefixer");
-const cleanCSS = require("gulp-clean-css");
-const postcss = require("gulp-postcss");
+let path = {
+  build: {
+    html: project_folder + "/",
+    css: project_folder + "/css/",
+    js: project_folder + "/js/",
+    img: project_folder + "/img/",
+    fonts: project_folder + "/fonts/"
+  },
+  src: {
+    html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
+    css: source_folder + "/scss/style.scss",
+    allscss: source_folder + "/scss/**/.scss",
+    js: source_folder + "/js/main.js",
+    img: source_folder + "/img/**/*.+(jpg|png|svg|gif|ico|webp)",
+    fonts: source_folder + "/fonts/*.ttf"
+  },
+  watch: {
+    html: source_folder + "/**/*.html",
+    css: source_folder + "/scss/**/*.scss",
+    js: source_folder + "/js/**/*.js",
+    img: source_folder + "/img/**/*.+(jpg|png|svg|gif|ico|webp)"
+  },
+  clean: "./" + project_folder + "/"
+}
 
-// const dist = "/Applications/MAMP/htdocs/test"; // Ссылка на вашу папку на локальном сервере
-const dist = "./dist";
+let { src, dest } = require("gulp"),
+  gulp = require("gulp");
+let browsersync = require("browser-sync").create();
+let fileinclude = require("gulp-file-include");
+let del = require("del");
+let scss = require("gulp-sass");
+let autoprefixer = require("gulp-autoprefixer");
+let group_media = require("gulp-group-css-media-queries");
+let clean_css = require("gulp-clean-css");
+let rename = require("gulp-rename");
+let imagemin = require("gulp-imagemin");
+let ttf2woff = require("gulp-ttf2woff");
+let ttf2woff2 = require("gulp-ttf2woff2");
+let uglify = require("gulp-uglify-es").default;
 
-gulp.task("copy-html", () => {
-    return gulp.src("./src/index.html")
-                .pipe(gulp.dest(dist))
-                .pipe(browsersync.stream());
-});
 
-gulp.task("build-sass", () => {
-    return gulp.src("./src/sass/style.scss")
-                .pipe(sass().on('error', sass.logError))
-                .pipe(gulp.dest(dist))
-                .pipe(browsersync.stream());
-});
+function browserSync(params) {
+  browsersync.init({
+    server: {
+      baseDir: "./" + project_folder + "/"
+    },
+    port: 3000,
+    directory: true,
+    notify: false
+  })
+}
 
-gulp.task("build-js", () => {
-    return gulp.src("./src/js/main.js")
-                .pipe(webpack({
-                    mode: 'development',
-                    output: {
-                        filename: 'script.js'
-                    },
-                    watch: false,
-                    devtool: "source-map",
-                    module: {
-                        rules: [
-                          {
-                            test: /\.m?js$/,
-                            exclude: /(node_modules|bower_components)/,
-                            use: {
-                              loader: 'babel-loader',
-                              options: {
-                                presets: [['@babel/preset-env', {
-                                    debug: true,
-                                    corejs: 3,
-                                    useBuiltIns: "usage"
-                                }]]
-                              }
-                            }
-                          }
-                        ]
-                      }
-                }))
-                .pipe(gulp.dest(dist))
-                .on("end", browsersync.reload);
-});
+function html() {
+  return src(path.src.html)
+    .pipe(fileinclude())
+    .pipe(dest(path.build.html))
+    .pipe(browsersync.stream())
+}
 
-gulp.task("watch", () => {
-    browsersync.init({
-		server: "./dist/",
-		port: 4000,
-		notify: true
-    });
-    
-    gulp.watch("./src/index.html", gulp.parallel("copy-html"));
-    gulp.watch("./src/js/**/*.js", gulp.parallel("build-js"));
-    gulp.watch("./src/sass/**/*.scss", gulp.parallel("build-sass"));
-});
+function css() {
+  return src(path.src.css)
+    .pipe(
+      scss({
+        outputStyle: "expanded"
+      })
+    )
+    .pipe(
+      group_media()
+    )
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["Last 4 versions"],
+        cascade: true
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(clean_css())
+    .pipe(
+      rename({
+        extname: ".min.css"
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream())
+}
 
-gulp.task("build", gulp.parallel("copy-html", "build-js", "build-sass"));
+function js() {
+  return src(path.src.js)         // путь к исходникам
+    .pipe(fileinclude())          // собираем подключаемые файлы скриптов в один файл
+    .pipe(dest(path.build.js))    // выгружаем в конечную папку
+    .pipe(
+      uglify()                    // минимизируем и оптимизируем файл скриптов
+    )
+    .pipe(
+      rename({                    // переименовываем минимизированный файл
+        extname: ".min.js"
+      })
+    )
+    .pipe(dest(path.build.js))    // выгружаем переименованный и минимизированный файл в конечную папку
+    .pipe(browsersync.stream())   // обновляем браузер
+}
 
-gulp.task("prod", () => {
-    gulp.src("./src/sass/style.scss")
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss([autoprefixer()]))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest(dist));
+function images() {
+  return src(path.src.img)
+    .pipe(
+      imagemin({
+        progressive: true,
+        svgoPlugins: [{ removeViewBox: false }],
+        interlaced: true,
+        optimizationLevel: 3 // 0 to 7
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream())
+}
 
-    return gulp.src("./src/js/main.js")
-                .pipe(webpack({
-                    mode: 'production',
-                    output: {
-                        filename: 'script.js'
-                    },
-                    module: {
-                        rules: [
-                          {
-                            test: /\.m?js$/,
-                            exclude: /(node_modules|bower_components)/,
-                            use: {
-                              loader: 'babel-loader',
-                              options: {
-                                presets: [['@babel/preset-env', {
-                                    corejs: 3,
-                                    useBuiltIns: "usage"
-                                }]]
-                              }
-                            }
-                          }
-                        ]
-                      }
-                }))
-                .pipe(gulp.dest(dist));
-});
+function fonts() {
+  return src(path.src.fonts)
+    .pipe(ttf2woff())
+    .pipe(dest(path.build.fonts))
+    .pipe(src(path.src.fonts))
+    .pipe(ttf2woff2())
+    .pipe(dest(path.build.fonts))
+}
 
-gulp.task("default", gulp.parallel("watch", "build"));
+function watchFiles(params) {
+  gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.css], css);
+  gulp.watch([path.watch.js], js);
+  gulp.watch([path.watch.img], images);
+}
+
+function clean(params) {
+  return del(path.clean);
+}
+
+let build = gulp.series(clean, gulp.parallel(css, js, html, images, fonts));
+
+let watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.fonts = fonts;
+exports.images = images;
+exports.js = js;
+exports.css = css;
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
