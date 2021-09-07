3f5069f7e505a477c4220ec7dba6fd62b5f100a5
vv_tomilin <backatom1@gmail.com>
2021-09-07 12:48:53 +0500

    Commit 10.2 (реализовал закрытие меню при нажатии на один из элементов списка меню)

--- src/js/modules/headerMenuToggle.js
+++ src/js/modules/headerMenuToggle.js
@@ -2,18 +2,33 @@ const headerMenuToggle = () => {
 
 	const menuToggle = document.querySelector('.header__menu-toggle');
 	const modalBlock = document.querySelector('.header__menu-mobile-modal');
+	const menuItems = document.querySelectorAll('.header__menu-mobile-modal-item');
 
-	menuToggle.addEventListener('click', function () {
+	menuToggle.addEventListener('click', () => {
 		if (menuToggle.classList.contains('header__menu-toggle--closed')) {
-			menuToggle.classList.remove('header__menu-toggle--closed');
-			menuToggle.classList.add('header__menu-toggle--opened');
-			modalBlock.classList.remove('header__menu-mobile-modal--hide');
+			openMenu();
 		} else {
-			menuToggle.classList.add('header__menu-toggle--closed');
-			menuToggle.classList.remove('header__menu-toggle--opened');
-			modalBlock.classList.add('header__menu-mobile-modal--hide');
+			closeMenu();
 		}
 	});
+
+	menuItems.forEach((item) => {
+		item.addEventListener('click', () => {
+			closeMenu();
+		});
+	});
+
+	function openMenu() {
+		menuToggle.classList.remove('header__menu-toggle--closed');
+		menuToggle.classList.add('header__menu-toggle--opened');
+		modalBlock.classList.remove('header__menu-mobile-modal--hide');
+	}
+
+	function closeMenu() {
+		menuToggle.classList.add('header__menu-toggle--closed');
+		menuToggle.classList.remove('header__menu-toggle--opened');
+		modalBlock.classList.add('header__menu-mobile-modal--hide');
+	}
 }
 
 export default headerMenuToggle;
