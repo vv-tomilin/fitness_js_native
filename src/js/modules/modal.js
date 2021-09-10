ddedeffce0edf20379c9244dd49c969c12b07e8e
vv_tomilin <backatom1@gmail.com>
2021-09-10 13:44:48 +0500

    Commit 13 (реализовал остановку прокрутки при открытии модального окна, убрал дергание страницы)

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -4,10 +4,14 @@ const modal = () => {
 	const triggers = document.querySelectorAll('[data-modal-open]');
 	const close = document.querySelector('.popup__close');
 
+	const scroll = calcScroll();
+
 	triggers.forEach((trigger) => {
 		trigger.addEventListener('click', () => {
 			if (modal.classList.contains('popup--hide')) {
 				modal.classList.remove('popup--hide');
+				document.body.style.overflow = 'hidden';
+				document.body.style.marginRight = `${scroll}px`;
 			}
 		});
 	});
@@ -15,8 +19,26 @@ const modal = () => {
 	close.addEventListener('click', () => {
 		if (!modal.classList.contains('popup--hide')) {
 			modal.classList.add('popup--hide');
+			document.body.style.overflow = '';
+			document.body.style.marginRight = '0px';
 		}
 	});
+
+	function calcScroll() {
+    const div = document.createElement('div');
+
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidded';
+
+    document.body.appendChild(div);
+    const scrollWidth = div.offsetWidth - div.clientWidth;
+
+    div.remove();
+
+    return scrollWidth;
+  }
 };
 
 export default modal;
